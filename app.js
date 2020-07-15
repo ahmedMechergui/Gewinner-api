829781732a720594d88e5ac261fa79497ba43aaa
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-07-16 00:28:16 +0100

    deploy

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const visitorRouter = require('./routes/visitor-router');
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
