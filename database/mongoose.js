fc2519de6465e63075be73e1f3a0a7b6e2c6fdc1
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-07-16 13:05:11 +0100

    d

--- database/mongoose.js
+++ database/mongoose.js
@@ -5,7 +5,7 @@ const databaseLink = process.env.MONGOLAB_URI ;
 // connecting to database
 // mongoose.connect('mongodb://127.0.0.1:27017/gewinner-api', {
 // mongoose.connect('mongodb+srv://ahmed:9ar9ouch@gewinner.xsbnq.mongodb.net/gewinner?retryWrites=true&w=majority', {
-mongoose.connect(databaseLink, {
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/gewinner-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => {
