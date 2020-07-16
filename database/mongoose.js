9492dfa03ae5754788309574be18ba01880189d8
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-07-16 12:36:56 +0100

    debug 2

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
+const databaseLink = process.env.MONGOLAB_URI ;
+
 // connecting to database
 // mongoose.connect('mongodb://127.0.0.1:27017/gewinner-api', {
 // mongoose.connect('mongodb+srv://ahmed:9ar9ouch@gewinner.xsbnq.mongodb.net/gewinner?retryWrites=true&w=majority', {
-mongoose.connect('mongodb+srv://ahmed:9ar9ouch@gewinner.xsbnq.mongodb.net/gewinner?retryWrites=true&w=majority', {
+mongoose.connect(databaseLink, {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => {
