add3894b0a4b675d182cb74a28ca0403429e440a
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-07-16 15:43:07 +0100

    d

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // connecting to database
-// mongoose.connect('mongodb+srv://ahmed:9ar9ouch@gewinner.xsbnq.mongodb.net/gewinner?retryWrites=true&w=majority', {
-mongoose.connect('mongodb://127.0.0.1:27017/gewinner-api', {
+mongoose.connect('mongodb+srv://ahmed:9ar9ouch@gewinner.xsbnq.mongodb.net/gewinner?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb://127.0.0.1:27017/gewinner-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => {
