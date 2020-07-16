8ccf9d58a189ba75f463667d7a2b270748439f33
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-07-16 12:03:54 +0100

    connected to new database

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // connecting to database
-mongoose.connect('mongodb://127.0.0.1:27017/gewinner-api', {
+// mongoose.connect('mongodb://127.0.0.1:27017/gewinner-api', {
+mongoose.connect('mongodb+srv://ahmed:9ar9ouch@gewinner.xsbnq.mongodb.net/gewinner?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => {
